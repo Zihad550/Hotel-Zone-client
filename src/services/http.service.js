c8889897c6120b47f3fb4765d351a70453c86fb4
Zihad550 <dev.jehadhossain@gmail.com>
2022-10-22 20:28:24 +0600

    fix: update timeout for free hosting

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 const axiosConfig = {
     baseURL: process.env.REACT_APP_API_BASE_URL,
-    timeout: 25000,
+    // best is to set 15000
+    timeout: 35000, // because of using free hosting platforms
 }
 
 const axiosInstance = axios.create(axiosConfig);
